4ee353e17d54b11ad43cae138ea1462828814c2a
Chooben <benmchoo@gmail.com>
2025-02-15 04:39:33 -0500

    Fill test table with more data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const cardData = [
-  { name: "Discover", grocery: 1, restaraunt: 5 },
-  { name: "Chase", grocery: 2, restaraunt: 1 }
+  { name: "Discover", grocery: "1%", restaraunt: "5%", amazon: "-" },
+  { name: "Chase", grocery: "2%", restaraunt: "1%", amazon: "-"},
+  { name: "Citi", grocery: "1%", restaraunt: "-", amazon: "5%"}
 ]
 
 function App() {
@@ -18,13 +19,15 @@ function App() {
             <th>Card</th>
             <th>Grocery</th>
             <th>Restaraunt</th>
+            <th>Amazon</th>
           </tr>
           {cardData.map((val, key) => {
             return (
               <tr key={key}>
                 <td className="cardName">{val.name}</td>
-                <td>{val.grocery}%</td>
-                <td>{val.restaraunt}%</td>
+                <td>{val.grocery}</td>
+                <td>{val.restaraunt}</td>
+                <td>{val.amazon}</td>
               </tr>
             )
           })}
