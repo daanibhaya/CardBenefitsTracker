65ace3c76b8b383c465f6c53d851bc0a324be969
Chooben <benmchoo@gmail.com>
2025-02-15 04:23:03 -0500

    Basic table format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,33 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const cardData = [
+  { name: "Discover", grocery: 1, restaraunt: 5 },
+  { name: "Chase", grocery: 2, restaraunt: 1 }
+]
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="cardTable">
+        <table>
+          <tr>
+            <th>Card</th>
+            <th>Grocery</th>
+            <th>Restaraunt</th>
+          </tr>
+          {cardData.map((val, key) => {
+            return (
+              <tr key={key}>
+                <td className="cardName">{val.name}</td>
+                <td>{val.grocery}%</td>
+                <td>{val.restaraunt}%</td>
+              </tr>
+            )
+          })}
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
 export default App
